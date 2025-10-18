b8e208e6bf0a00ba4c08c50af354e66feab411b2
MiVece <141875054+MiVece@users.noreply.github.com>
2025-10-18 18:52:13 +0200

    Delete Úkol_1 directory

--- Úkol_1/index.js
+++
@@ -1,4 +0,0 @@
-const userPrompt = Number(prompt("Zadejte teplotu ve stupních Fahrenheita."));
-const resultCalculated = (userPrompt - 32) * (5/9);
-const resultRounded = resultCalculated.toFixed(1).replace('.', ',');
-document.body.innerHTML += `<h1>${userPrompt} °F je ${resultRounded} °C.`;
