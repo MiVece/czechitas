44eeed13bf68d5b54928bfa8a93f9623d68239f8
MiVece <misa.vecerkova@gmail.com>
2025-10-18 23:44:31 +0200

    Added a condition to catch NaN.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const userPrompt = Number(prompt("Zadejte teplotu ve stupních Fahrenheita."));
+
+if (isNaN(userPrompt)) {
+    document.body.innerHTML += "<h1>Zadejte prosím číselnou hodnotu.</h1>";
+} else {
 const resultCalculated = (userPrompt - 32) * (5/9);
 const resultRounded = resultCalculated.toFixed(1).replace('.', ',');
-document.body.innerHTML += `<h1>${userPrompt} °F je ${resultRounded} °C.`;
+document.body.innerHTML += `<h1>${userPrompt} °F je ${resultRounded} °C</h1>.`;
+}
