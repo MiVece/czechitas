945238b92b53778826ca9fcd97c8ef9f6b1d8cfb
MiVece <141875054+MiVece@users.noreply.github.com>
2025-11-07 21:53:58 +0100

    Update index.js
    Committing second homework.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const userPrompt = Number(prompt("Zadejte teplotu ve stupních Fahrenheita."));
-
-if (isNaN(userPrompt)) {
-    document.body.innerHTML += "<h1>Zadejte prosím číselnou hodnotu.</h1>";
-} else {
-const resultCalculated = (userPrompt - 32) * (5/9);
-const resultRounded = resultCalculated.toFixed(1).replace('.', ',');
-document.body.innerHTML += `<h1>${userPrompt} °F je ${resultRounded} °C</h1>.`;
+let prvni_cislo = Number(prompt("Zadejte první číslo:"));
+while (isNaN(prvni_cislo)) {
+  prvni_cislo = Number(prompt("To není číslo. Zkuste to znovu:"));
 }
+
+let druhe_cislo = Number(prompt("Zadejte druhé číslo:"));
+while (isNaN(druhe_cislo)) {
+  druhe_cislo = Number(prompt("To není číslo. Zkuste to znovu:"));
+}
+
+let treti_cislo = Number(prompt("Zadejte třetí číslo:"));
+while (isNaN(treti_cislo)) {
+  treti_cislo = Number(prompt("To není číslo. Zkuste to znovu:"));
+}
+
+function max3 (a,b,c) {
+    if (a>b && a>c)
+        return a
+    else if (b>a && b>c)
+        return b
+    else
+        return c
+};
+
+document.body.innerHTML +=  `Největší ze zadaných čísel je <span class="numberStyle">${max3(prvni_cislo, druhe_cislo, treti_cislo)}</span>.`;
+
